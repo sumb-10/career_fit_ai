230248cd510e2e304bcc34ef6d79f14482aea705
hojin.cho <saku07891@gmail.com>
2026-07-06 13:44:11 +0900

    add react/vite frontend base

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default {
+ content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+ theme: { extend: {} },
+ plugins: [],
+}
